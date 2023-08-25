30feb67153d1473705ba6e1f95d473e9de0d1dba
Hamburger <luishamburger1152@hotmail.com>
2023-08-24 20:48:04 -0500

    feature/funcion-transporte

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('transporte')
+  getTransport(): string {
+    return 'Transporte nuevo';
+  }
 }
